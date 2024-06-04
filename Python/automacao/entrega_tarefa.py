25a9a6a069ed785aaf0267f4d56f9571f5387161
Anderson Junior <juninho16carvalho@gmail.com>
2024-06-04 20:06:30 -0300

    '

--- Python/automacao/entrega_tarefa.py
+++ Python/automacao/entrega_tarefa.py
@@ -2,13 +2,17 @@ import pyautogui as pa
 import time
 import os
 
-pa.PAUSE = 2
+pa.PAUSE = 1
 
+edge = 'C:/Users/0220482413040/Documents/pessoal/pessoal/Python/automacao/edge.png'
+
+local_edge = pa.locateCenterOnScreen(edge, confidence=0.8)
+
+pa.click(local_edge.x, local_edge.y)
 
 
 teams = 'C:/Users/0220482413040/Documents/pessoal/pessoal/Python/automacao/teams.png'
 
+local_teams = pa.locateCenterOnScreen(teams, confidence=0.8)
 
-local = pa.locateCenterOnScreen(teams, confidence=0.8)
-
-pa.click(local.x, local.y)
+pa.click(local_teams.x, local_teams.y)
